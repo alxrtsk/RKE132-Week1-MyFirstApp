6837df954bba0d75c9c676cf4cfb56ba45375d90
alexr <reitsak.alex@gmail.com>
2024-01-30 21:12:15 +0200

    myfirstapp

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a commentary for the code
+
+// App asks the user's name
+// App greets user by it's name
+
+Console.WriteLine("Enter your name:"); //output
+//string=sõne
+string userName = Console.ReadLine(); //input
+
+Console.WriteLine("Hello" + ", " +  userName + "!");
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !");
